3ac252c12a5e9ef9a6b54c17dcbaf9f03e96464c
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-08 20:23:06 +0000

    Create 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+//Write a script that prints the first argument passed to it:
+
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(arg);
+}
